4c9df142c6804b86b9c39086db7ac4af2f162d97
Zxc <dsdsfdssddsdfs@yandex.ru>
2026-04-16 20:21:03 +0300

    Add files via upload

--- Help me/main.py
+++ Help me/main.py
@@ -6,15 +6,18 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.core.window import Window
 import json
+
+
 Window.size = (350, 500)
 
-def Click():
-    print('Click is working')
-    
 
 class MyApp(App):
     def build(self):
-        txt = Label(text='Text')
+        self.text = Label(text='0', font_size=40, size_hint=(1, 1))
+        self.current_input = ''
+        self.result_shown = False
+
+
         btn1 = Button(text='1')
         btn2 = Button(text='2')
         btn3 = Button(text='3')
@@ -32,8 +35,29 @@ class MyApp(App):
         btn15 = Button(text='/')
         btn16 = Button(text='=')
         btn17 = Button(text='DEL')
-        btn18 = Button(text='Test button')
-        btn18.on_press = Click
+        btn18 = Button(text='CLR')
+ 
+
+        btn1.on_press = self.press_1
+        btn2.on_press = self.press_2
+        btn3.on_press = self.press_3
+        btn4.on_press = self.press_plus
+        btn5.on_press = self.press_4
+        btn6.on_press = self.press_5
+        btn7.on_press = self.press_6
+        btn8.on_press = self.press_minus
+        btn9.on_press = self.press_7
+        btn10.on_press = self.press_8
+        btn11.on_press = self.press_9
+        btn12.on_press = self.press_tochka
+        btn13.on_press = self.press_0
+        btn14.on_press = self.press_ymozhenie
+        btn15.on_press = self.press_delenie
+        btn16.on_press = self.calculate
+        btn17.on_press = self.delete_last
+        btn18.on_press = self.delete_all
+
+
 
         main = BoxLayout(orientation='vertical')
 
@@ -44,7 +68,7 @@ class MyApp(App):
         layout3 = BoxLayout(orientation='horizontal')
         layout4 = BoxLayout(orientation='horizontal')
 
-        layout_text.add_widget(txt)
+        layout_text.add_widget(self.text)
         layout.add_widget(btn1)
         layout.add_widget(btn2)
         layout.add_widget(btn3)
@@ -73,5 +97,137 @@ class MyApp(App):
         main.add_widget(layout3)
         main.add_widget(layout4)
         return main
+
+    
+    def update_font_size(self):
+        text_lenght = len(self.text.text)
+
+        if text_lenght <= 8:
+            new_size = 40
+        elif text_lenght <= 10:
+            new_size = 35
+        elif text_lenght <= 12:
+            new_size = 30
+        elif text_lenght <= 14:
+            new_size = 25
+        elif text_lenght <= 16:
+            new_size = 20
+        elif text_lenght <= 18:
+            new_size = 18
+        elif text_lenght <= 20:
+            new_size = 16
+        else:
+            new_size = 14
+        self.text.font_size = new_size
+
+
+    def press_1(self):
+        self.add_number('1')
+    
+    def press_2(self):
+        self.add_number('2')
+
+    def press_3(self):
+        self.add_number('3')
+
+    def press_4(self):
+        self.add_number('4')
+
+    def press_5(self):
+        self.add_number('5')
+
+    def press_6(self):
+        self.add_number('6')
+
+    def press_7(self):
+        self.add_number('7')
+
+    def press_8(self):
+        self.add_number('8')
+
+    def press_9(self):
+        self.add_number('9')
+
+    def press_0(self):
+        self.add_number('0')
+
+    def press_tochka(self):
+        self.add_number('.')
+
+    
+
+    def press_plus(self):
+        self.add_operator('+')
+
+    def press_minus(self):
+        self.add_operator('-')
+    
+    def press_ymozhenie(self):
+        self.add_operator('*')
+    
+    def press_delenie(self):
+        self.add_operator('/')
+
+
+
+
+
+    def add_number(self, num):
+        if self.result_shown:
+            self.current_input = ''
+            self.result_shown = False
+        self.current_input += num
+        if self.current_input:
+            self.text.text = self.current_input
+        else:
+            self.text.text = '0'
+        self.update_font_size()
+
+    def add_operator(self, op):
+        if self.current_input and self.current_input[-1] not in '+-*/':
+            self.current_input += op
+            self.text.text = self.current_input
+            self.result_shown = False
+            self.update_font_size()
+
+    def delete_last(self):
+        self.current_input = self.current_input[:-1]
+        if self.current_input:
+            self.text.text = self.current_input
+        else:
+            self.text.text = '0'
+        self.result_shown = False
+        self.update_font_size()
+
+    def delete_all(self):
+        self.current_input = ''
+        self.text.text = '0'
+        self.result_shown = False
+        self.update_font_size()
+
+    def calculate(self):
+        try:
+            if not self.current_input:
+                return
+            if '/0' in self.current_input:
+                self.text.text = 'Error'
+                self.current_input = ''
+                self.result_shown = True
+                return
+            
+            result = eval(self.current_input)
+            if isinstance(result, float):
+                result = round(result, 10)
+                if result == int(result):
+                    result = int(result)
+            
+            self.text.text = str(result)
+            self.current_input = str(result)
+            self.result_shown = True 
+            self.update_font_size()
+        except:
+            self.text.text = 'Error'
+            self.current_input = ''
+            self.result_shown = True
 
 MyApp().run()
